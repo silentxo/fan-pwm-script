915e7ee1a7be0896c462d03d71e5f288bc5199ed
silentxo <wzl161919@gmail.com>
2024-01-18 14:51:17 +0800

    Create install.sh
    bat file for cp and active daemon

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo cp -f ./x735-fan.sh /usr/local/bin/
+sudo cp -f ./x735-fan.service /lib/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable x735-fan
+sudo systemctl start x735-fan
